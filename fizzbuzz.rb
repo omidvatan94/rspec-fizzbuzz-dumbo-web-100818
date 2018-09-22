190199b865d96d33d04bc256bf05f44c15b1ba77
Omid Vatandoost <omidvatan94@gmail.com>
2018-09-22 21:41:17 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(int)
-  if int%3 == 0
-    return "Fizz"
-  else if int%5 == 0
-    return "Buzz"
+def fizz_buzz(num)
+  if num % 3 == 0 && num % 5 == 0
+    "fizzbuzz"
+  elsif num % 3 == 0
+    "fizz"
+  elsif num % 5 == 0
+    "buzz"
   else
-    return "FizzBuzz"
-  end
-  end
+    num
   end
 end
