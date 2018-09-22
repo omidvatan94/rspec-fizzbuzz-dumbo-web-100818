37fc684580ea08dc0e005ab95c9279a290fb3b3a
Omid Vatandoost <omidvatan94@gmail.com>
2018-09-22 21:41:27 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizz_buzz(num)
+def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
     "fizzbuzz"
   elsif num % 3 == 0
